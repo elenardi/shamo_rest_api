2621c99a5757876b29d7bf0d0a6609ec7b288eaa
Elen Ardiyanto <elard.yanto@gmail.com>
2023-01-04 15:52:16 +0700

    update env

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,8 +4,8 @@ require('dotenv').config()
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
