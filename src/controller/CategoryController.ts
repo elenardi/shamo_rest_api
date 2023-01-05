7b60270e53db87b4aaebf46ac2291633a36e553b
Elen Ardiyanto <elard.yanto@gmail.com>
2023-01-05 13:35:21 +0700

    get all category

--- src/controller/CategoryController.ts
+++ src/controller/CategoryController.ts
@@ -65,7 +65,21 @@ export const updateCategory = async(request: Request, response: Response, next: 
 
         await categoryRepository.save(setCategory)
 
-        return response.status(201).send(successResponse('Success update category', {data: setCategory}, 201))
+        return response.status(200).send(successResponse('Success update category', {data: setCategory}, 200))
+    } catch (error) {
+        return response.status(400).send(errorResponse(error, 400))
+    }
+}
+
+export const getAllCategory = async(request: Request, response: Response, next: NextFunction) => {
+    try {
+        if (request.jwtPayload.role !== UserRole.ADMIN){
+            return response.status(405).send(errorResponse("Don't have access", 405))
+        }
+
+        const category = await categoryRepository.find()
+
+        return response.status(200).send(successResponse('Success update category', {data: category}, 200))
     } catch (error) {
         return response.status(400).send(errorResponse(error, 400))
     }
--- src/routes/v1/category.ts
+++ src/routes/v1/category.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { createCategory, updateCategory } from '../../controller/CategoryController'
+import { createCategory, getAllCategory, updateCategory } from '../../controller/CategoryController'
 import { checkJwt } from '../../utils/checkJwt'
 
 const router = Router()
 
 router.post('/create', [checkJwt, createCategory])
 router.patch('/update/id=:id', [checkJwt, updateCategory])
+router.get('/all', [checkJwt, getAllCategory])
 
 export default router
